b98ec249b14782255111fa075da40c07d39f47d1
Vincent Song <songvc@users.noreply.github.com>
2015-12-22 23:27:45 -0800

    delete

--- scripts/main.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(document).ready(function(){$(".open-panel").click(function(){$("html").addClass("openNav")}),$(".close-panel, #content").click(function(){$("html").removeClass("openNav")}),$(".btn").click(function(e){e.preventDefault(),$(".modal").is(":visible")?$(".modal").hide():$(".modal").show()}),$(".close").click(function(e){e.preventDefault(),$(".modal").hide()})});
